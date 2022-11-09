471b020b5b70f33d54d046f47046c1af32f5bbec
Razieh <raziehamouzgar@gmail.com>
2022-11-09 10:42:29 +0330

    adding MealItemForm

--- src/component/Meals/MealItem/MealItem.js
+++ src/component/Meals/MealItem/MealItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './MealItem.module.css';
-
+import MealItemForm from './MealItemForm';
 const MealItem = (props) => {
     const price = `${props.price.toFixed(3)}`;
     return (
@@ -11,7 +11,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-
+<MealItemForm />
             </div>
         </li>
     );
---
+++ src/component/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="تعداد "
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "6",
+          step: "1",
+          defaultValue: "0",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
